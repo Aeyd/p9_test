34d2beb3699a44ef86cec46057005c472c3b23af
Aeyd <adi.pali@hotmail.com>
2023-09-29 23:29:48 +0200

    Update weblate_push.sh

--- weblate_push.sh
+++ weblate_push.sh
@@ -5,7 +5,7 @@ applications_files=(artifacts/Application/*/*.json)
 
 for filename in "${applications_files[@]}"
 do
-   translations=$(jq '.objects[]? | {name: .fieldName, translations: .attributes[]?.translation[]?} ' $filename)
+   translations=$(jq '.objects[]? | {(.fieldName): .attributes[]?.translation[]?.value} ' $filename)
    echo "$translations"
 done 
 
