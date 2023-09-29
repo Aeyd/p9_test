32c22e8d88cc60b51e848a8180526c04d307ea8e
Aeyd <adi.pali@hotmail.com>
2023-09-29 20:47:32 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+shopt -s nullglob
+applications_files=(artifacts/Application/*/*.json)
+
+declare -a applications_data
+
+for filename in "${applications_files[@]}"
+do
+   value=$(<$filename)
+   applications_data+=("$value")
+done 
+
+JSON_STRING=$( jq -n \
+               --arg appl "$applications_data" \
+               '{applications: $appl}' )
+			 
+echo "$JSON_STRING"
