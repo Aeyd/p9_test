35bd765915719f8721bc65de25dde1904b7947cf
Aeyd <adi.pali@hotmail.com>
2023-09-29 22:29:33 +0200

    Create weblate_push.sh

---
+++ weblate_push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+shopt -s nullglob
+applications_files=(artifacts/Application/*/*.json)
+
+for filename in "${applications_files[@]}"
+do
+   translations=$(jq '.objects[] | {name: .fieldName, translations: .attributes[].translation[]} ' $filename)
+   echo "$translations"
+done 
+
