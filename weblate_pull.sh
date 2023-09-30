cdf11abbea825fa7664befa2e6cf9e2b1a527ca2
Aeyd <adi.pali@hotmail.com>
2023-09-30 15:37:07 +0200

    Update weblate_pull.sh

--- weblate_pull.sh
+++ weblate_pull.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+shopt -s nullglob
+translation_files=(translations/*.json)
+
+for filename in "${translation_files[@]}"
+do
+  while IFS= read -r line
+  do
+    echo "$line"
+  done < "$filename"
+done 
 
