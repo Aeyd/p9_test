75ad66b7f81180ec591e5b7d3919df2b6e31d8d7
Aeyd <adi.pali@hotmail.com>
2023-09-30 16:22:08 +0200

    Update weblate_pull.sh

--- weblate_pull.sh
+++ weblate_pull.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
 shopt -s nullglob
-translation_files=(translations/*.json)
+translation_path=(translations/*.json)
+source_path="artifacts/Application/test_app_deployment/test_app_deployment.json"
+source_data=$(<$source_path)
 
-for filename in "${translation_files[@]}"
+for path in "${translation_path[@]}"
 do
+  lang = ${path: -6:2}
+  echo $lang
+  tmp=$(mktemp)
+  tmp=$(<$source_path)
+  
   while IFS= read -r line
   do
-    echo "$line"
-  done < "$filename"
-done 
+    IFS=- read -r key value <<< $line
+    jq --arg k="$key" /
+       --arg v="$value" /
+       --arg l="$lang" /
+      '.objects[]? | select(.fieldname == $k ) | .attributes[]?.translation[]? | select(.language == $l ) | .value |= $v' /
+      $tmp > "$tmp"
+  done < "$path"
 
+  mv "$tmp" "$source_path"
+done 
